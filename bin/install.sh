b87bb19cccb734efcea474f29c9ee4d0e8d73583
benghaziboy <cam.m.lowe@gmail.com>
2018-11-07 17:34:26 -0500

    Addition: site provision & plugin script

---
+++ bin/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z "$WP_URL" ]]; then
+  WP_URL="http://localhost:8888"
+fi
+
+if [[ -z "$WP_TITLE" ]]; then
+  WP_TITLE="SteveLand"
+fi
+
+if [[ -z "$WP_USER" ]]; then
+  WP_USER="admin"
+fi
+
+if [[ -z "$WP_PASSWORD" ]]; then
+  WP_PASSWORD="very_weak_password"
+fi
+
+if [[ -z "$WP_EMAIL" ]]; then
+  WP_EMAIL="email@example.com"
+fi
+
+docker-compose run cli wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_USER} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL}
+docker-compose run cli wp plugin activate gutenberg
+docker-compose run cli wp plugin activate SteveGutenberg
