9542f7dd98bb0b80872e210abef9b1cd2c218ea9
benghaziboy <cam.m.lowe@gmail.com>
2018-11-08 13:22:02 -0500

    Refactor: run wp-cli specific script inside container

--- bin/install.sh
+++ bin/install.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-if [[ -z "$WP_URL" ]]; then
-  WP_URL="http://localhost:8888"
-fi
-
-if [[ -z "$WP_TITLE" ]]; then
-  WP_TITLE="SteveLand"
-fi
-
-if [[ -z "$WP_USER" ]]; then
-  WP_USER="admin"
-fi
-
-if [[ -z "$WP_PASSWORD" ]]; then
-  WP_PASSWORD="very_weak_password"
-fi
-
-if [[ -z "$WP_EMAIL" ]]; then
-  WP_EMAIL="email@example.com"
-fi
-
-docker-compose run cli /bin/bash -c "wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_USER} --admin_password=${WP_PASSWORD} --admin_email=${WP_EMAIL} &&
-  wp plugin activate gutenberg &&
-  wp plugin activate SteveGutenberg"
+docker-compose run cli
---
+++ bin/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker-compose up -d wordpress 
+
+# TODO: 
+# 
+# This is a workaround to prevent the wp-cli from running before the mysql
+# application is available.
+# 
+# Ideally we should be checking the status until mysql is available.
+sleep 10;
+. ./bin/install.sh
